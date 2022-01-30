25023a7ff69dea91f782e415f15dbafab006fefc
Andrey <upndrey@yandex.ru>
2022-01-30 16:24:40 +0700

    fullscreen

--- js/index.js
+++ js/index.js
@@ -5,9 +5,6 @@ function main() {
 
     const services = new Services();
     services.init();
-
-    const fullscreenImage = new FullscreenImage();
-    fullscreenImage.init();
 }
 
 function Burger() {
@@ -22,27 +19,6 @@ Burger.prototype.init = function() {
     });
 };
 
-function FullscreenImage() {
-
-};
-
-FullscreenImage.init = function() {
-    $(".fullscreen").click(function(){	// Событие клика на маленькое изображение
-        var img = $(this);	// Получаем изображение, на которое кликнули
-      var src = img.attr('src'); // Достаем из этого изображения путь до картинки
-      $("body").append("<div class='popup'>"+ //Добавляем в тело документа разметку всплывающего окна
-                       "<div class='popup_bg'></div>"+ // Блок, который будет служить фоном затемненным
-                       "<img src='"+src+"' class='popup_img' />"+ // Само увеличенное фото
-                       "</div>");
-      $(".popup").fadeIn(200); // Медленно выводим изображение
-      $(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
-          $(".popup").fadeOut(200);	// Медленно убираем всплывающее окн	
-    setTimeout(function() {	// Выставляем таймер
-            $(".popup").remove(); // Удаляем разметку всплывающего окна
-          }, 200);
-      });
-  });
-}
 
 function Services() {
     this.brandStrategyLink = document.getElementById("brandStrategyLink");
